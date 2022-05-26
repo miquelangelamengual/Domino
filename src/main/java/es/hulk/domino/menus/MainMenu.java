7ba0b46ab550760ff04db2391d8872f76cad2515
Miquel Angel <hulkrojo2@gmail.com>
2022-05-26 09:18:45 +0200

    first iteration with the interface

--- src/main/java/es/hulk/domino/menus/MainMenu.java
+++ src/main/java/es/hulk/domino/menus/MainMenu.java
@@ -1,4 +1,26 @@
 package es.hulk.domino.menus;
 
+import lombok.Getter;
+
+import java.util.Scanner;
+
 public class MainMenu {
+
+    @Getter private static String gameType;
+
+    public void printMenu() {
+        Scanner scanner = new Scanner(System.in);
+        switch (scanner.nextLine()) {
+            case "INTERNATIONAL":
+                gameType = "INTERNATIONAL";
+                break;
+            case "COLOMBIAN":
+                gameType = "COLOMBIAN";
+                break;
+            case "LATIN":
+                gameType = "LATIN";
+                break;
+        }
+    }
+
 }
