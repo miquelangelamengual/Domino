103261ca75f04ce5b3219c480c9d74cbc2951329
Miquel Angel <hulkrojo2@gmail.com>
2022-05-26 09:17:54 +0200

    added instances for gameloader and main menu on main class

--- src/main/java/es/hulk/domino/Domino.java
+++ src/main/java/es/hulk/domino/Domino.java
@@ -1,9 +1,20 @@
 package es.hulk.domino;
 
+import es.hulk.domino.games.GameLoader;
+import es.hulk.domino.menus.MainMenu;
+import lombok.Getter;
+
 public class Domino {
 
-    public static void main(String[] args) {
+    @Getter private static GameLoader gameLoader;
+    @Getter private static MainMenu mainMenu;
 
+    public static void main(String[] args) {
+        mainMenu = new MainMenu();
+        gameLoader = new GameLoader();
+
+        mainMenu.printMenu();
+        gameLoader.load();
     }
 
 }
