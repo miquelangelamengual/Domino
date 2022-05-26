77d765ebc636bf74f7e950c2f36a76bb5f7c1fd7
Miquel Angel <hulkrojo2@gmail.com>
2022-05-26 09:18:29 +0200

    added first gameloader (this will change in a future)

--- src/main/java/es/hulk/domino/games/GameLoader.java
+++ src/main/java/es/hulk/domino/games/GameLoader.java
@@ -1,4 +1,31 @@
 package es.hulk.domino.games;
 
+import es.hulk.domino.games.impl.Colombian;
+import es.hulk.domino.games.impl.International;
+import es.hulk.domino.games.impl.Latin;
+import es.hulk.domino.menus.MainMenu;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class GameLoader {
+
+    private String selectedGame = MainMenu.getGameType();
+
+    private GameInterface gameInterface;
+    private static String gameType;
+
+    public void load() {
+        if (MainMenu.getGameType().equals("INTERNATIONAL")) {
+            setGameInterface(new International());
+            gameType = "International";
+        } else if (MainMenu.getGameType().equals("COLOMBIAN")) {
+            setGameInterface(new Colombian());
+            gameType = "Colombian";
+        } else if (MainMenu.getGameType().equals("LATIN")) {
+            setGameInterface(new Latin());
+            gameType = "LATIN";
+        }
+    }
+
 }
