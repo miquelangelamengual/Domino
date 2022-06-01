752d1f6b1ea9f4b669023b7f16bf11ff3205aa5e
Miquel Angel <hulkrojo2@gmail.com>
2022-06-01 11:30:27 +0200

    added token object and added toString to print 1 token

--- src/main/java/es/hulk/domino/board/Board.java
+++ src/main/java/es/hulk/domino/board/Board.java
@@ -1,4 +1,12 @@
 package es.hulk.domino.board;
 
+import es.hulk.domino.token.Token;
+
 public class Board {
+
+    public static void printBoard() {
+        Token token = new Token("0");
+        System.out.print(token.toString());
+    }
+
 }
--- src/main/java/es/hulk/domino/token/Token.java
+++ src/main/java/es/hulk/domino/token/Token.java
@@ -1,4 +1,28 @@
 package es.hulk.domino.token;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter @Setter
 public class Token {
+
+    private String name;
+
+    @Override
+    public String toString() {
+
+        if (name.equalsIgnoreCase("0")) {
+            return  "*******\n" +
+                    "*     *\n" +
+                    "*     *\n" +
+                    "*******\n" +
+                    "*     *\n" +
+                    "*     *\n" +
+                    "*******";
+        }
+
+        return "";
+    }
 }
