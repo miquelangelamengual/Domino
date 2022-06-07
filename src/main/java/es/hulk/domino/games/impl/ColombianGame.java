d68ba59cb4a1a4c6f35da1df470896a6f285f6c5
Miquel Angel <hulkrojo2@gmail.com>
2022-06-07 09:49:22 +0200

    added first board (experimental) on colombian

--- src/main/java/es/hulk/domino/games/impl/ColombianGame.java
+++ src/main/java/es/hulk/domino/games/impl/ColombianGame.java
@@ -1,28 +1,51 @@
 package es.hulk.domino.games.impl;
 
-import es.hulk.domino.board.Board;
 import es.hulk.domino.games.GameInterface;
 import es.hulk.domino.player.Player;
 import es.hulk.domino.player.PlayerManager;
 import es.hulk.domino.token.Card;
+import es.hulk.domino.utils.Text;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ColombianGame implements GameInterface {
 
+    List<Card> playedCards = new ArrayList<>();
+
     @Override
     public void electionMenu() {
-        Board.printBoard();
+        displayGame();
         for (Player player : PlayerManager.getPlayerList()) {
             Scanner scanner = new Scanner(System.in);
+            System.out.println();
             System.out.println("Player " + player.getName() + " choose a card to play");
-            int cardIndex = scanner.nextInt();
+            int cardIndex = scanner.nextInt() - 1;
             Card card = player.getHand().get(cardIndex);
             player.getHand().remove(cardIndex);
-            System.out.println(card.toString());
-
-            if (player.getHand().isEmpty()) break;
+            playedCards.add(card);
+            for (Card playCard : playedCards) {
+                System.out.print(playCard);
+            }
+            if (player.getHand().isEmpty() || player.getHand().size() == 0) Text.gameWon(player);
         }
         this.electionMenu();
+    }
+
+    public void displayGame() {
+        for (Player player : PlayerManager.getPlayerList()) {
+            System.out.println();
+            System.out.println(player.getName() + " deck");
+            System.out.println();
+            for (Card card : player.getHand()) {
+                System.out.println(card.toString());
+            }
+            System.out.println();
+            System.out.println();
+        }
+        for (Card playCard : playedCards) {
+            System.out.print(playCard);
+        }
     }
 }
