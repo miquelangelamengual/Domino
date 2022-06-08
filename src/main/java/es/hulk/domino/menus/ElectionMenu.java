7e35a7117566331f3dad7374663e9e066b2e8baf
Miquel Angel <hulkrojo2@gmail.com>
2022-06-08 10:25:36 +0200

    finished ElectionMenu

--- src/main/java/es/hulk/domino/menus/ElectionMenu.java
+++ src/main/java/es/hulk/domino/menus/ElectionMenu.java
@@ -17,31 +17,83 @@ public class ElectionMenu {
 
     private final List<Card> playedCard = new ArrayList<>();
     private final GameLoader gameLoader = Domino.getGameLoader();
-    public void selectSide() {
-        gameLoader.getGameInterface().displayGame();
+
+    public void chooseOption() {
         for (Player player : PlayerManager.getPlayerList()) {
-            Text.printSideSelection(player);
-            int option = ErrorCatching.returnChoseInt(0, 2);
-            int cardIndex = ErrorCatching.returnParseInt() - 1;
-            Card card = player.getHand().get(cardIndex);
-            player.getHand().remove(cardIndex);
+            Text.chooseElection(player);
+            int option = ErrorCatching.returnChoseInt(0, 3);
 
-            if (option == 1) {
-                playedCard.add(0, card);
-            } else if (option == 2) {
-                playedCard.add(card);
-            } else {
-                System.out.println("OPCION NO VALIDA PESTUZO");
+            switch (option) {
+                case 1 -> putCard(player);
+                case 2 -> takeCard(player);
+                case 3 -> Text.tornLeave();
+                case 4 -> Text.exit();
             }
-
-            for (Card playCard : playedCard) {
-                System.out.print(playCard);
-            }
-
-            if (player.getHand().isEmpty()) Text.gameWon(player);
         }
+        this.chooseOption();
+    }
+
+    public void takeCard(Player player) {
+        if (player.getHand().size() > 0) {
+            int option = ErrorCatching.returnChoseInt(0, player.getHand().size() - 1);
+            Card card = player.getHand().get(option);
+            playedCard.add(card);
+            player.getHand().remove(card);
+        } else {
+            Text.noCard();
+        }
+    }
+
+    public void putCard(Player player) {
         gameLoader.getGameInterface().displayGame();
-        this.selectSide();
+        Text.printSideSelection(player);
+        int option = ErrorCatching.returnChoseInt(0, 2);
+        int cardIndex = ErrorCatching.returnParseInt() - 1;
+        Card card = player.getHand().get(cardIndex);
+
+        switch (option) {
+            case 1:
+                if (playedCard.size() == 0) {
+                    playedCard.add(card);
+                    player.getHand().remove(card);
+                } else if (card.getSecondNumber() == playedCard.get(0).getFirstNumber()) {
+                    playedCard.add(0, card);
+                    player.getHand().remove(card);
+                } else {
+                    card.rotate();
+                    if (card.getSecondNumber() == playedCard.get(0).getFirstNumber()) {
+                        playedCard.add(0, card);
+                        player.getHand().remove(card);
+                    } else {
+                        Text.invalidCard();
+                    }
+                }
+                break;
+            case 2:
+                if (playedCard.size() == 0) {
+                    playedCard.add(card);
+                    player.getHand().remove(card);
+                } else if (card.getSecondNumber() == playedCard.get(playedCard.size() - 1).getFirstNumber()) {
+                    playedCard.add(card);
+                    player.getHand().remove(card);
+                } else {
+                    card.rotate();
+                    if (card.getSecondNumber() == playedCard.get(playedCard.size() - 1).getFirstNumber()) {
+                        playedCard.add(card);
+                        player.getHand().remove(card);
+                    } else {
+                        Text.invalidCard();
+                    }
+                }
+                break;
+        }
+
+        for (Card playCard : playedCard) {
+            System.out.print(playCard);
+        }
+
+        if (player.getHand().isEmpty()) Text.gameWon(player);
+        gameLoader.getGameInterface().displayGame();
     }
 
 }
